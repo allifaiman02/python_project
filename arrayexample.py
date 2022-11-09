c584b34569cabe39ebbc02480c9568c4f9a07a3a
Allif Aiman <allifaiman0212@gmail.com>
2022-11-10 07:58:32 +0800

    loop

--- arrayexample.py
+++ arrayexample.py
@@ -9,14 +9,30 @@ sum = 0
 #   if i >= len(list) - 1  :
 #      print("There is no more item")
 
-while i < len(list)  :
-    print("I is "+str(i))
-    sum += list[i]
-    i += 1
-    print("Current sum is "+str(sum))
+#while i < len(list)  :
+ #   print("I is "+str(i))
+ #   sum += list[i]
+ #   i += 1
+ #   print("Current sum is "+str(sum))
 
-    if i > len(list)  :
-        print("There is no more item")
+ #  if i > len(list)  :
+ #      print("There is no more item")
 
+# print("Total number is "+str(sum))
 
-print("Total number is "+str(sum))
+r = 100
+
+j = 0
+n = 0
+
+for j in range(r):
+    n += 1
+    print("N is " + str(n))
+
+k = 0
+s = 0
+
+while k < r :
+    k += 1
+    s += 1 
+    print("S is "+ str(s))
