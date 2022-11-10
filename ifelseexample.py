2ce8203dd15cc0012abf1fbf510f33b24aaa8040
Allif Aiman <allifaiman0212@gmail.com>
2022-11-10 08:06:36 +0800

    if else

---
+++ ifelseexample.py
@@ -0,0 +1,13 @@
+a = 0
+b = 0
+
+a = int(input("Please insert the value for a : "))
+b = int(input("Please insert the value for b : "))
+
+
+if a > b :
+    print ("a is larger than b")
+elif a == b :
+    print ("a and b is equal")
+else :
+    print ("b is larger than a")
