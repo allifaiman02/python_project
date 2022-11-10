e1f4fb2facb55b2ed5c9b8327c42d31a9a656b9e
Allif Aiman <allifaiman0212@gmail.com>
2022-11-10 09:09:33 +0800

    dictionary

---
+++ dictionaryexample.py
@@ -0,0 +1,18 @@
+employee = {}
+
+max_length = 1
+
+while len(employee) < max_length :
+    name = input("Enter employee's name : ")
+    #salary = int(input("Enter employee's salary : "))
+    
+    work_exp = []
+    for j in range (3) :
+        work = input("Enter your work experince :")
+        work_exp.append(work) 
+
+    if name not in employee :
+        #employee[name] = salary
+        employee[name] = work_exp
+
+print(employee)
