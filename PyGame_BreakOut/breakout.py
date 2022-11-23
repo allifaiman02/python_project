79b3c679b86a52f4211060aff4c530906b35c230
Allif Aiman <allifaiman0212@gmail.com>
2022-11-23 14:20:30 +0800

    block done

--- PyGame_BreakOut/breakout.py
+++ PyGame_BreakOut/breakout.py
@@ -7,7 +7,88 @@ screen_width = 600
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame
+pygame.display.set_caption("Breakout")
+
+
+#define the colours
+bg = (234, 218, 184)
+#block colours
+block_red = (242, 85, 96)
+block_green = (86, 174, 87)
+block_blue = (69, 177, 232)
+
+#define game variable
+cols = 6
+rows = 6
+
+#brick wall class
+class wall():
+    def __init__(self):
+        self.width = screen_width // cols
+        self.height = 50
+
+    def create_wall(self):
+        self.blocks = []
+        #define an empty list for an individual block
+        block_individual = []
+        for row in range(rows):
+            #reset the block row list
+            block_row = []
+            #iterate through each colum in that row
+            for col in range(cols):
+                #generate x and y position for each block and create a rectangle for that
+                block_x = col * self.width
+                block_y = row * self.height
+                rect = pygame.Rect(block_x, block_y, self.width, self.height)
+                #assign block strength based on row
+                if row < 2: 
+                    strength = 3
+                elif row < 4:
+                    strength = 2
+                elif row < 6:
+                    strength = 1
+                #create a list at this point to store the rect and colour data
+                block_individual = [rect, strength]
+                #append that individual block in to block row
+                block_row.append(block_individual)
+            #append the row to the full list of blocks
+            self.blocks.append(block_row)
+
+    def draw_wall(self):
+        for row in self.blocks:
+            for block in row :
+                #assign the colour based on the strength 
+                if block[1] == 3:
+                    block_col = block_blue
+                elif block[1] == 2:
+                    block_col = block_green
+                elif block[1] == 1:
+                    block_col = block_red
+                pygame.draw.rect(screen, block_col, block[0])
+                pygame.draw.rect(screen, bg, (block[0]), 2)
+
+
+#creat a wall
+wall = wall()
+wall.create_wall()
+
+
+run = True
+while run:
+
+    screen.fill(bg)
+
+    #draw wall 
+    wall.draw_wall()
+
+    for event in pygame.event.get():
+        
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
 
 
 
