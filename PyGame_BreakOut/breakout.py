410023a1e6a19d7cc53229dd9b71917f7b05d1a1
Allif Aiman <allifaiman0212@gmail.com>
2022-11-23 13:03:08 +0800

    pygame

---
+++ PyGame_BreakOut/breakout.py
@@ -0,0 +1,15 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen_width = 600
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame
+
+
+
+
+
